fd5d2f3fe2a61bfb321df868184646d44e7882b8
lucas55i <lucas55i@outlook.com>
2025-12-07 09:05:03 -0300

    output

--- modules.tf
+++ modules.tf
@@ -5,21 +5,12 @@ module "network" {
   cidr_block   = var.cidr_block
 }
 
-# module "elastiCache" {
-#   source              = "./modules/elastiCache"
-#   vpc_id              = module.network.vpc_id
-#   subnet_ids          = [module.network.subnet_priv_1a, module.network.subnet_priv_1b]
-#   allowed_cidrs       = module.network.vpc_cidr
-#   replication_group_id = "meu-redis-ha"
-#   node_type           = "cache.t3.micro"
-#   num_nodes           = 2
-# }
 module "elastiCache" {
   source                = "./modules/elastiCache"
   vpc_id                = module.network.vpc_id
   subnet_ids            = [module.network.subnet_priv_1a, module.network.subnet_priv_1b]
   allowed_cidrs         = [module.network.vpc_cidr]
-  replication_group_id  = "meu-redis-ha"
+  replication_group_id  = "elastic-cache-redis"
   node_type             = "cache.t3.micro"
   replicas_per_node_group = 1 # 1 replica (total = 2 nós)
   engine_version          = "7.1"
--- modules/elastiCache/outputs.tf
+++ modules/elastiCache/outputs.tf
@@ -2,3 +2,7 @@ output "redis_endpoint" {
   description = "Endpoint do Redis"
   value       = aws_elasticache_replication_group.redis_rg.primary_endpoint_address
 }
+
+output "sg_ingress_rules" {
+  value = data.aws_security_group.redis_sg.id
+}
